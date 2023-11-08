613c7ae477d60490c887c8994098a1e9bf3315b9
Moawiyah <moawiyah.nat@gmail.com>
2023-11-08 13:29:05 +0200

    post data form

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const http = require('http');
-const port = 3000;
+const fs = require('fs');
+const port = 4000;
 
 const server = http.createServer((req, res) => {
-    res.setHeader('content-Type', 'text')
-    res.write("Welcome to vast!");
-    res.end();
+
+    res.setHeader('content-Type', 'text/html')
+    console.log(req.url);
+
+    if (req.url == "/") {
+        fs.readFile('./view/index.html', (err, data) => {
+            if (err) {
+                console.log(err);
+                res.end();
+            } else {
+                res.write(data);
+                res.end();
+            }
+        });
+    } else if (req.url == "/person" && req.method == "POST") {
+        res.end("The data was received successfully");
+    }
+
 })
 
 server.listen(port, 'localhost', () => {
